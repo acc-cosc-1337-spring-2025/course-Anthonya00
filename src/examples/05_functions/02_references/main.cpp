5fdb820157dd48e067c748c492937f7ad23f585c
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2025-02-27 00:29:20 +0000

    intro to reference variables

--- src/examples/05_functions/02_references/main.cpp
+++ src/examples/05_functions/02_references/main.cpp
@@ -5,13 +5,21 @@ using std::cout;
 
 int main()
 {
-    auto num = 10; auto num1 = 20;
+    auto num = 10; 
+    //referrence variable
+    auto &num_ref = num; //num_ref will hold the address of num
+    
+    
+    cout<<"Num value: "<<num<<"\n"; //display the value of num
+    cout<<"Num variable is at address: "<<&num<<"\n";//memory operator & reference
 
+    cout<<"value of num via num_ref: "<<num_ref<<"\n";//display the value of the memory address you store
+    cout<<"Address that num_ref sotres: "<<&num_ref<<"\n";
+
+    num_ref = 20;
     cout<<"Num value: "<<num<<"\n";
-    cout<<"Num variable is at address: "<<&num<<"\n";//memory operator &
+    cout<<"value of num via num_ref: "<<num_ref<<"\n";
 
-    cout<<"Num 1 value: "<<num1<<"\n";
-    cout<<"Num1 variable is at address: "<<&num1<<"\n";
 
     return 0;
 }
