de2c3630e6b36c5e01eacf9ac5d6a97e62686348
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2025-03-04 00:02:19 +0000

    reference value and parameters

--- test/examples/05_functions/05_functions_tests.cpp
+++ test/examples/05_functions/05_functions_tests.cpp
@@ -1,6 +1,33 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "reference.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+
+TEST_CASE("Test reference variable")
+{
+	auto num1 = 5;
+	int &num2 = num1;
+
+	REQUIRE(NUM1 ==  num2);
+	num2 = 10; //num2 has the address of num1 thus we can modify 
+
+	REQUIRE(num1 == 10);
+
+}
+
+TEST_CASE("Test value/reference parameters (variables)")
+{
+	int num1 = 5;//x100
+	int num2 = 10;//x96
+
+	ref_value_vars(num1, num2);//num2 changes the value of memory address x96 -. num2 form line 34
+
+	REQUIRE(num1 == 5);
+	REQUIRE(num1 == 20);
+
+
 }
