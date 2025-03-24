5486635c779c9efcc8a56c64b67fbf8d1b9176d4
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2025-03-24 23:29:55 +0000

    Intro to class structure ex- bank account

--- src/examples/08_classes/01_bank/bank_account.h
+++ src/examples/08_classes/01_bank/bank_account.h
@@ -1,1 +1,12 @@
 //bank_account.h
+
+class BankAccount
+{
+//access specifier variables/functions
+public://outside code class/fucitons can use variables and functions that are public
+    BankAccount(int b) : balance(b) {}//class contructor
+    int get_balance(){return balance;}
+private://only this class can use variables/functions
+    int balance{};
+
+};
