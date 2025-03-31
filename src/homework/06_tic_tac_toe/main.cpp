7162458c4ee8ccce430aae9be4cc6ab08c878364
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2025-03-31 22:32:26 +0000

    HW#6 pt. 2

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,40 @@
+#include "tic_tac_toe.h"
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::string;
+
+
 
 int main() 
 {
+
+	TicTacToe game; 
+	string goku;
+	char user_choice = 'y';
+
+	do
+	{
+		cout<<"First Player press X or O ";
+		cin>>goku;
+
+		game.start_game(goku);
+
+		int position;
+
+		while(!game.game_over())
+		{
+			cout<< "Enter your position";
+			cin>>position;
+			game.mark_board(position);
+			game.display_board();
+		}
+
+		cout<< "Play again, enter y or Y? ";
+		cin>>user_choice;
+
+	}while(user_choice == 'y' ||user_choice == 'Y');
 
 	return 0;
 }
