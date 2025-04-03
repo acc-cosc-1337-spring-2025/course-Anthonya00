77adefd854fa0ed2f6d92ea5d0cda74bad38243a
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2025-04-03 02:16:02 +0000

    HW#7

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -16,7 +16,7 @@ int main()
 
 	do
 	{
-		cout<<"First Player press X or O ";
+		cout<<"First Player press X or O: ";
 		cin>>goku;
 
 		game.start_game(goku);
@@ -25,13 +25,13 @@ int main()
 
 		while(!game.game_over())
 		{
-			cout<< "Enter your position";
+			cout<< "Enter your position: ";
 			cin>>position;
 			game.mark_board(position);
 			game.display_board();
 		}
 
-		cout<< "Play again, enter y or Y? ";
+		cout<< "Play again, enter Y or N? ";
 		cin>>user_choice;
 
 	}while(user_choice == 'y' ||user_choice == 'Y');
--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -13,10 +13,16 @@ class TicTacToe{
         void mark_board(int position);
         string get_player() const{return X_or_O;};
         void display_board() const;
+        string get_winner();
     private:
         void set_X_or_O();
         bool check_board_full();
         void clear_board();
         string X_or_O;
+        string winner = "none"; 
+        bool check_colum_win();
+        bool check_row_win();
+        bool check_diagonal_win();
+        void set_winner();
 };
 
