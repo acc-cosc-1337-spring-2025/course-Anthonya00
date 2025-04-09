ba911174fd28d89bdb8138943ff63f6da95cf5a7
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2025-04-09 22:57:55 +0000

    get heap data from a pointer

--- src/examples/11_memory/02_dynamic_memory/main.cpp
+++ src/examples/11_memory/02_dynamic_memory/main.cpp
@@ -1,5 +1,18 @@
+#include<iostream>
+#include "dynamic_memory.h"
+
+using std::cout;
+
 int main() 
 {
 	
+	int* num = get_dynamic_memory_bank_account();
+	cout<<num<<"\n";
+
+	delete num;
+	num = nullptr;
+	
+
+
 	return 0;
 }
