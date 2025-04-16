314c94a34e1b0c3c7312a176876bd9b8e7e747ac
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2025-04-16 22:55:33 +0000

    atm w unique ptr to BanAccount

--- src/examples/08_classes/01_bank/atm.cpp
+++ src/examples/08_classes/01_bank/atm.cpp
@@ -1,5 +1,10 @@
 //atm.cpp
 #include "atm.h"
+#include "checkingaccount.h"
+#include "savings_account.h"
+#include <iostream>
+#include vector
+#include memory 
 
 using std::cout;
 using std::cin;
@@ -41,3 +46,47 @@ BankAccount& get_reference_account()
     BankAccount account(500);
     return account;
 }
+
+void run_menu()
+{
+    //list of uniqque ptr to accounts 
+    auto account_index = 0, bank_menu_choice=0;
+
+    vector<unique_ptr<BankAccount>> accounts ; 
+    accounts.push_back(make_unique<CheckingAccout>(500));
+    accounts.push_back(make_unique<SavingsAccount>(750));
+    
+
+    //loop prompt user for checking or savings 
+
+    do {
+        cout<<"Enter 1 menu choice: ";
+        cin account_index;
+
+        auto* account = accountw [account_index-1].get();
+        ATM atm (account);
+
+    }
+
+    do{
+
+        //loop dplauy menu handle user options
+        do 
+        {
+            
+
+                /* code*/
+        }while (bank_menu_choice != 4 );
+    }
+
+}
+
+void display_menu()
+
+{
+    cout<<"\n"
+
+
+}
+
+void handle_menu_option(ATM& atm, int menu_choice)
--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -13,22 +13,9 @@ using std::cin;
 
 int main()
 {	
-	vector<unique_ptr<BankAccount>> accounts; 
-	unique_ptr<BankAccount> checking = make_unique<CheckingAccount>(500);
-	unique_ptr<BankAccount> svaings = make_unique<SavingsAccount>(1000);
 
-	cout<<"checking get balance "<<checking-> get_balance()<<"\n";
-	cout<<"savings get balance "<<savings->get_balance()<<"\n";
 
-	accounts.push_back(std::move(checking));
-	accounts.push_back(std::move(savings));
-
-	//cout<<"checking get balance "<<checking-> get_balance()<<"\n"; ERRORS OUR//JCHECKING POINTS TO INVALID MEMORY
-
-	for (auto &account: accounts)
-	{
-		cout<<account_>get_balance()<<"\n";
-	}
+	run_menu();
 
 
 	return 0;
