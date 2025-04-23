bb3533eef653ecc288ee2392abcf47d6c9b75ab8
Anthonya00 <143046693+Anthonya00@users.noreply.github.com>
2025-04-23 22:40:17 +0000

    Stack array

--- src/examples/10_arrays/01_arrays_mem/arrays_mem.h
+++ src/examples/10_arrays/01_arrays_mem/arrays_mem.h
@@ -1,3 +1,11 @@
 //arrays_mem.h - stack array example
+#include <iostream>
 
 //function prototype for stack_array
+void create_stack_array()
+{
+
+    const int SIZE = 3;
+    int nums[SIZE]{6,1,10};
+
+}
--- src/examples/10_arrays/01_arrays_mem/main.cpp
+++ src/examples/10_arrays/01_arrays_mem/main.cpp
@@ -1,9 +1,9 @@
-
+#include "arrays_mem.h"
 
 int main() 
 {
 
-	
+	create_stack_array();
 
 	return 0;
 
